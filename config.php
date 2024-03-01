07b11e1b958d6b1adc4857c1d3afc27263e33b4c
Anonimoes <work@cedricverlinden.com>
2024-03-01 01:13:09 +0100

    Add config.php file met import aliases

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Import aliases
+define('ROOT', $_SERVER['DOCUMENT_ROOT']);
+define('PUBLIC', ROOT . '/public/');
+define('SRC', ROOT . '/src/');
+define('COMPONENTS', SRC . '/components/');
+define('DEMOS', SRC . '/demos/');
+define('LIB', SRC . '/lib/');
+define('PAGES', SRC . '/pages/');
