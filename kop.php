bd09f99ac1f0a5a70fca1db2d369c65afe84a3b8
MRoggen <lode.roggen@bazandpoort.be>
2025-04-02 10:28:35 +0200

    Verwijderen van https en toevoegen van launch.json
    Https zou in Apache moeten geconfigureerd worden (en dus niet in de code). Dit laat toe om lokaal http servers te runnen.

--- kop.php
+++ kop.php
@@ -1,11 +1,4 @@
 <?php
-if(!isset($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] != "on")
-{
-    //Tell the browser to redirect to the HTTPS URL.
-    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"], true, 301);
-    //Prevent the rest of the script from executing.
-    exit;
-}
 date_default_timezone_set("Europe/Brussels");
 setlocale(LC_TIME, 'NL_nl');
 ?>
